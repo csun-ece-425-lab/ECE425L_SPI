8cf1d283033f2fac1e7599521227bd82c85c4de0
ananas-csun <ananas.ece25@gmail.com>
2025-04-04 02:11:56 -0700

    Added template for the SPI lab

---
+++ SPI/SSI0.c
@@ -0,0 +1,32 @@
+/**
+ * @file SSI0.c
+ *
+ * @brief Source code for the SSI0 driver.
+ *
+ * This file contains the function definitions for the SSI0 driver.
+ *
+ * @note For more information regarding the SSI module, refer to the
+ * Synchronous Serial Interface (SSI) section of the TM4C123GH6PM Microcontroller Datasheet.
+ *   - Link: https://www.ti.com/lit/gpn/TM4C123GH6PM
+ *
+ * @note Assumes that the system clock (50 MHz) is used.
+ *
+ * @author
+ */
+
+#include "SSI0.h"
+
+void SSI0_Init(void)
+{
+
+}
+
+uint8_t SSI0_Input_Data(void)
+{
+	return 0;
+}
+
+void SSI0_Output_Data(uint8_t data)
+{
+
+}
